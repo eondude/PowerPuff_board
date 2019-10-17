4f8d523e24881869c16129a82c93be6d1990f6ca
eondude <danj1@student.unimelb.edu.au>
2019-10-17 11:15:53 +1100

    integrated withHongyu's code. Getting NullPointerErrors

--- Distributed_board/src_1/EventThread.java
+++ Distributed_board/src_1/EventThread.java
@@ -32,27 +32,42 @@ public class EventThread implements Runnable {
 	                index++;
 			}
 		case "mouseReleased":
-					iArray[index]=event.drawing;
-					wb.repaint();
-					index++;
-			
+			iArray[index]=event.drawing;
+			wb.repaint();
+			index++;
+		
+		case "mouseNulled":
+			iArray[index] = new Line();
+			iArray[index].x1=iArray[index].x2=0;
+			iArray[index].y1=iArray[index].y2=1;
+			iArray[index].type = 4;
+	        iArray[index].R = 100;
+	        iArray[index].G = 100;
+	        iArray[index].B = 100;
+	        iArray[index].stroke = 1.0f;
+	        index++;
 		}
+		
 	}
 	
 	public void run() {
 		while (true) {
-			ArrayList<BoardEvent> bEvnts;
+			ArrayList<BoardEvent> bEvnts = new ArrayList<BoardEvent>();
+			BoardEvent event1=new BoardEvent("mouseNulled");
+			
+			
 			try {
 				bEvnts = wb.bServer.getBoardEvents(nextevntID);
-				
 				for (BoardEvent event : bEvnts) {
 					dispatchEvent(event);
 				}
-				
 				if (bEvnts.size()>0) {
+					
 					nextevntID=bEvnts.get(bEvnts.size() - 1).eventID +1;
 				}
 			}catch (RemoteException e) {
+				e.printStackTrace();
+			}catch (NullPointerException e) {
 				e.printStackTrace();
 			}
 		}
